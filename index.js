8129ca2008d8fdb76a4a577f3a65dd58ee28aa8d
gabrycina <c.gabriele.info@gmail.com>
2021-03-03 20:30:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+var path = require('path')
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
