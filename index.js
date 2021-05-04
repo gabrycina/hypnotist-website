e203de91cf722310fc3e918f4d678376f181d1a2
gabrycina <c.gabriele.info@gmail.com>
2021-05-04 15:43:00 +0100

    Added Mailchimp API connection and solved double-subscribing bug

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-"use strict";
-const express = require('express')
+require("dotenv").config();
+const http = require('http');
+const express = require('express');
 const app = express();
-const port = process.env.port || 8000;
+const port = process.env.port || 80;
+const mailchimp = require('./views/js/mailchimp');
 
 app.use(express.static('views'))
-
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   //handle root
@@ -20,6 +24,23 @@ app.get('/thankyou', (req, res) => {
 app.get('/test', (req, res) => {
   //handle root
   res.render('test');
+});
+
+app.post('/endpoint', async function(req, res) {
+  var name = req.body[0].value; 
+  var email = req.body[1].value;
+
+  var data = {}
+  data.name = name;
+  
+  try {
+    await mailchimp.addSubscriber(email, data, true);
+  }catch(err){
+    console.log("Mailhimp error: " + err);
+    return
+  }
+  
+  res.send({redirect: '/thankyou'});
 });
 
 app.listen(process.env.PORT || port, err => {
